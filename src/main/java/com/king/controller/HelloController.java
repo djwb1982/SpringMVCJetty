62dbb303b4dca4d867a85d2e7f5df11eb2bc6ede
king1982 <djwb1982@163.com>
2016-11-29 17:30:31 +0800

    springmvc jetty

---
+++ src/main/java/com/king/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.king.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+	 @RequestMapping(value = "/hello", method = RequestMethod.GET)
+	    public String hello(@RequestParam(value="name", required=false, defaultValue="King") String name, Model model) {
+	        model.addAttribute("name", name);
+	        return "hello";
+	    }
+}
